7b2b392d1ae377801387dc1e9a90e61ea35de647
davidmorrison-08-30 <21521009@gmail.uit.edu.vn>
2023-06-14 09:29:39 +0700

    Khoi Nguyen added customer.py defining Customer class in models branch

---
+++ store/models/customer.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Customer(models.Model):
+    username = models.CharField(max_length=250)
+    phone = models.CharField(max_length=10)
+    password = models.CharField(max_length=250)
+
+    #to save the data
+    def register(self):
+        self.save()
+
+
+    @staticmethod
+    def get_customer_by_username(username):
+        try:
+            return Customer.objects.get(username= username)
+        except:
+            return False
+
+
+    def isExists(self):
+        if Customer.objects.filter(username = self.username):
+            return True
+
+        return False
