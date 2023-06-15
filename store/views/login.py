2aff91b84e47b455e3eb197d5cc248352d39ef8e
TuongTran1301 <105165586+TuongTran1301@users.noreply.github.com>
2023-06-15 09:06:28 +0700

    Tran Khiet Tuong added login.py in views in view_template_login branch

---
+++ store/views/login.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render , redirect , HttpResponseRedirect
+from django.contrib.auth.hashers import  check_password
+from store.models.customer import Customer
+from django.views import View
+
+
+class Login(View):
+    return_url = None
+
+    def get(self, request):
+        Login.return_url = request.GET.get ('return_url')
+        return render (request, 'login.html')
+
+    def post(self, request):
+        username = request.POST.get ('username')
+        password = request.POST.get ('password')
+        customer = Customer.get_customer_by_username (username)
+
+        print (username, password)
+        return render (request, 'login.html', {'error': error_message})
+
+def logout(request):
+    request.session.clear()
+    return redirect('login')
