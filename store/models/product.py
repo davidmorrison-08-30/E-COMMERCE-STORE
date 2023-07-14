0644b45e250a8d4cf904f921a515721900ca372c
davidmorrison-08-30 <21521009@gmail.uit.edu.vn>
2023-07-14 18:52:29 +0700

    Khoi Nguyen modified Products class in models/product.py and added view and template for details page

--- store/models/product.py
+++ store/models/product.py
@@ -4,7 +4,8 @@ class Products(models.Model):
     name = models.CharField(max_length=250)
     price = models.IntegerField()
     category= models.ForeignKey(Category,on_delete=models.CASCADE,default=1 )
-    description= models.CharField(max_length=250, default='', blank=True, null= True)
+   # description= models.CharField(max_length=250, default='', blank=True, null= True)
+    aspects = models.JSONField()
     image= models.ImageField(upload_to='uploads/products/')
 
     @staticmethod
@@ -19,4 +20,4 @@ class Products(models.Model):
         if category_id:
             return Products.objects.filter (category=category_id)
         else:
-            return Products.get_all_products();
+            return Products.get_all_products()
