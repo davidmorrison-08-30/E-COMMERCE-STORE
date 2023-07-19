06a6ff7c56a00b6ca557a57c053f6ad49e5b2d64
davidmorrison-08-30 <21521009@gmail.uit.edu.vn>
2023-07-19 18:32:44 +0700

    Khoi Nguyen modified admin site's template

--- store/admin.py
+++ store/admin.py
@@ -3,7 +3,25 @@ from .models.product import Products
 from .models.category import Category
 from .models.customer import Customer
 from .models.orders import Order
+from django.utils.safestring import mark_safe
 
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ['name', 'username', 'password']
+
+class AdminOrder(admin.ModelAdmin):
+    list_display = ['customer', 'display_order', 'address', 'phone', 'date', 'display_products', 'sum_price', 'status']
+   # list_display_links = None  # Disable linking to the detail view
+
+    def display_order(self, obj):
+        return obj
+
+    display_order.short_description = 'Order'
+
+    def display_products(self, obj):
+        elements = obj.products
+        return mark_safe("<br>".join(str(element[3])+"x "+str(element[1]) for element in elements))
+
+    display_products.short_description = 'Products'
 
 class AdminProduct(admin.ModelAdmin):
     list_display = ['name', 'price', 'category']
@@ -15,5 +33,5 @@ class CategoryAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Products,AdminProduct)
 admin.site.register(Category)
-admin.site.register(Customer)
-admin.site.register(Order)
+admin.site.register(Customer, AdminCustomer)
+admin.site.register(Order, AdminOrder)
