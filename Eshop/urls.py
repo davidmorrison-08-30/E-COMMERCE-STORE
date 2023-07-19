e710e6e12dcaff58889d40d1f718a936c6863d2b
davidmorrison-08-30 <21521009@gmail.uit.edu.vn>
2023-07-19 12:15:10 +0700

    template for new admin site

--- Eshop/urls.py
+++ Eshop/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls.static import static
 from . import settings
 
 urlpatterns = [
+    path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
+    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
     path('admin/', admin.site.urls),
     path('' , include('store.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
