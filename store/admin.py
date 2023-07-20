138c3d099b0894062e56060a18eb3d6bcaf15c0f
TuongTran1301 <105165586+TuongTran1301@users.noreply.github.com>
2023-07-20 21:49:22 +0700

    Tuong designed view for Order model in admin panel
    Tuong designed view for Order model in admin panel

--- store/admin.py
+++ store/admin.py
@@ -3,7 +3,22 @@ from .models.product import Products
 from .models.category import Category
 from .models.customer import Customer
 from .models.orders import Order
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ['name', 'username', 'password']
+
+class AdminOrder(admin.ModelAdmin):
+    list_display = ['order', 'customer', 'address', 'phone', 'date', 'display_products', 'sum_price', 'status']
+   # list_display_links = None  # Disable linking to the detail view
+
+
+    def display_products(self, obj):
+        elements = obj.products
+        return mark_safe("<br>".join(str(element[3])+"x "+str(element[1]+", "+str(element[2])) for element in elements))
+
+    display_products.short_description = 'Products'
 
 class AdminProduct(admin.ModelAdmin):
     list_display = ['name', 'price', 'category']
@@ -13,7 +28,7 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name']
 
 # Register your models here.
-admin.site.register(Products,AdminProduct)
+admin.site.register(Products, AdminProduct)
 admin.site.register(Category)
-admin.site.register(Customer)
-admin.site.register(Order)
+admin.site.register(Customer, AdminCustomer)
+admin.site.register(Order, AdminOrder)
